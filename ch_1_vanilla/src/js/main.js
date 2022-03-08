8de0c52ab15426922bb3b142573a92292e727664
Mitjanit <42413333+mitjanit@users.noreply.github.com>
2022-03-08 09:58:48 +0100

    react curs

--- ch_1_vanilla/src/js/main.js
+++ ch_1_vanilla/src/js/main.js
@@ -1,5 +1,44 @@
 import '../styles/alternative.scss';
 
+let renderList = (users) => {
+  const container = document.querySelector('.injectionPlace')
+  console.log(container)
+  if(users){
+    users.forEach((i) => {
+      const {employee_name} = i;
+      console.log(employee_name);
+      let elem = document.createElement("li");
+      elem.innerHTML = employee_name;
+      container.appendChild(elem);
+    })
+  }
+}
+
+let aux = (label) => {
+  console.log("hi there 2")
+  const button = document.getElementById("myButton")
+  console.log(button.innerHTML)
+  button.innerHTML = `Toma ${label}`
+
+  //fetch("http://dummy.restapiexample.com/api/v1/employees")
+  fetch("../employees.json")
+      .then(response =>{
+        return response.json()
+      }).then(json =>{
+        const {data} = json;
+        console.log(data);
+        renderList(data);
+      }).
+      catch(error => {
+            console.log("error", error)
+  });
+
+}
+
+aux('Pep');
+
+/*
+
 function doLog(msg){
   console.log(msg);
 }
@@ -13,3 +52,5 @@ const test = () => {
 doLog('conventional function')
 doAnotherLog('not so conventional function')
 test()
+
+ */
